e603d9763a9d4bb1d318cfcb24b1512bb23fd206
Jed Schmidt <jed@jedbook.local>
2009-11-04 03:51:18 -0800

    fixed spacing

--- tmpl-node.js
+++ tmpl-node.js
@@ -5,19 +5,19 @@
 // http://ejohn.org/blog/javascript-micro-templating/
 
 var posix = require( "posix" ),
-		http = require( "http" ),
+    http = require( "http" ),
     concat = Array.prototype.concat,
     slice = Array.prototype.slice;
 
 process.mixin( exports, {
-	compile: compile,
-	load: load,
-	defaultContexts: []
+  compile: compile,
+  load: load,
+  defaultContexts: []
 });
 
 http.ServerResponse.prototype.render = function( name, c1, c2, etc ) {
-	var	args = Array.prototype.slice.call( arguments ),
-			template = exports[ args.shift() ];
+  var args = Array.prototype.slice.call( arguments ),
+      template = exports[ args.shift() ];
 
   this.sendBody( template.apply( null, args ) );
   this.finish();
